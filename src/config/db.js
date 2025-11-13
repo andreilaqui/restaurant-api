030ee099083ecbd4e1e7eac7584bd52dbfacd7ba
Andrei Laqui <a.laqui837@mybvc.ca>
2025-11-13 12:25:23 -0700

    still testing

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');   // wrapper around MongoDB - so I can use schema, models, helpers
 
 async function connectDB() {
+  console.log(process.env.MONGODB_URI);
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB with Mongoose');
   } catch (err) {
-    console.log(process.env.MONGODB_URI);
+    
     console.error('MongoDB connection error! Error message:', err);
     process.exit(1);
   }
