b32dd093f28bdf625655ae70044d9a23f85bc3ed
Andrei Laqui <a.laqui837@mybvc.ca>
2025-11-20 12:29:08 -0700

    added express validator to auth

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,21 +2,43 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
 //schema
 const User = require('../models/userModel');
 
 
 // C-reate / signup
-router.post('/signup', async (req, res) => {
-  try {
+router.post(
+  '/signup',
+  [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('password')
+      .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+      .matches(/[A-Z]/)
+      .withMessage('Password must contain at least one uppercase letter')
+      .matches(/[0-9]/)
+      .withMessage('Password must contain at least one number')
+      .matches(/[^A-Za-z0-9]/)
+      .withMessage('Password must contain at least one special character'),
+    body('email').isEmail().withMessage('Invalid email address').optional({ nullable: true, checkFalsy: true }),
+    body('phone').isMobilePhone().withMessage('Invalid phone number').optional({ nullable: true, checkFalsy: true })
+  ],
+
+  async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array() }); }
+
     const { username, password, email, phone, role } = req.body;
-    const user = new User({ username, password, email, phone, role });
-    await user.save();
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+
+    try {
+      const user = new User({ username, password, email, phone, role });
+      await user.save();
+      res.status(201).json({ message: 'User created successfully' });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
 
 // login - this is not CRUD Create but using post to send sensitive data (password))
 router.post('/login', async (req, res) => {
